7608f781a39861e997a485260d30235d05e6243c
Jakub Kemr <kemr.jakub@gmail.com>
2026-01-22 13:51:24 +0100

    Přidání 5. otázky

--- constants.ts
+++ constants.ts
@@ -5,7 +5,8 @@ export const QUESTIONS: Question[] = [
   { text: "Chcete si koupit Kayenský pepř, kde byste ho hledali?", target: "Kayenský pepř" },
   { text: "Chcete si na večer koupit dobrý bourbon, kde byste ho hledali?", target: "Bourbon" },
   { text: "Máte chuť na vodní meloun, kde byste ho hledali?", target: "Vodní meloun" },
-  { text: "Chcete dětem koupit těstoviny ve tvaru zvířátek, kde byste je hledali?", target: "Zvířátkové těstoviny" }
+  { text: "Chcete dětem koupit těstoviny ve tvaru zvířátek, kde byste je hledali?", target: "Zvířátkové těstoviny" },
+  { text: "Chcete si koupit ovesné vločky, kde byste je hledali?", target: "Ovesné vločky" }
 ];
 
 export const ADMIN_PASSWORD = 'productdesign123';
